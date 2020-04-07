383bc40e4043ca2c5ca8f4425481f0289c3f8108
Matt Grofsky <matt@ytel.com>
2020-04-07 23:11:13 +0000

    Added GNU Snippet

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+"""
+Covid-19 Compliance Module
+Designed to be a Google Cloud Function that is triggered off a file upload to a Google Cloud Storage 
+bucket and notify a slack channel if certain Covid-19 keywords are detected within the audio.
+https://ask.ytel.com/covid-19-carrier-compliance
+
+Copyright (C) 2020  Ytel, Inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 import requests
 import json
 import os
